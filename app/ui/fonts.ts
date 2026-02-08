cf9c1d7ee890fa99ff3c3d1d8fa8860ca6715252
boxed-mikann <u085421i@ecs.osaka-u.ac.jp>
2026-02-08 12:43:50 +0900

    vercelのtutorial

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Roboto, Lusitana } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+export const subfont = Roboto({ weight: '400', subsets: ['latin'] });
+export const lusitana = Lusitana({ weight: '700', subsets: ['latin'] });
---
+++ next.config.ts
@@ -0,0 +1,18 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  turbopack: {
+    root: '/workspace/nextjs-dashboard/',
+  },
+  // webpack: (config, context) => {
+  //   config.watchOptions = {
+  //     poll: 1000,
+  //     aggregateTimeout: 500
+  //   }
+
+  //   return config
+  // },
+};
+
+export default nextConfig;
